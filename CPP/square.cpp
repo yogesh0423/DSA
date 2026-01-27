4504584ee954db4a9aa6908e9fa404ab5322f48e
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-01-27 07:05:57 +0000

    here i solve the problem of finding square of numbers

---
+++ CPP/square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    int square = n * n;
+    cout << "Square of " << n << " is: " << square << endl;
+    
+    return 0;
+}
