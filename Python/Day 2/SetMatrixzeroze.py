c79bf7c4f822626a2f7681a443a1be0802a0e867
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-03 15:36:16 +0000

    Deleted Python folder

--- Python/Day 2/SetMatrixzeroze.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        col0 = 1
-
-        for i in range(rows):
-            if matrix[i][0] == 0:
-                col0 = 0
